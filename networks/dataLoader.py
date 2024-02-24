182daa5da48787dac1fab5336992b35d11b857f9
nicolas <nicolaspermanne@gmail.com>
2024-02-24 19:02:49 +0100

    Adaptation of the dataloader to load tensor instead of hashmap

--- networks/dataLoader.py
+++ networks/dataLoader.py
@@ -5,8 +5,8 @@ import os
 import numpy as np
 
 # PATH (should be the same that in model/dataset_generator.py
-DATASET_FOLDER_PATH = "dataset"
-DATASET_FOLDER_NAME = "{}_start={}_interval={}_ndraw={}"
+DATASET_FOLDER_PATH = "datasets"
+DATASET_FOLDER_NAME = "{}_start={}_interval={}_ndraw={}_size=({},{})"
 DATASET_FILE_NAME = "dataset.csv"
 DATA_NAME = "image{}_type={}_time={}.npy"
 CELLS_TYPES = "cells_types"
@@ -14,15 +14,17 @@ CELLS_DENSITIES = "cells_densities"
 OXYGEN = "oxygen"
 GLUCOSE = "glucose"
 
+
 class SimulationDataset(Dataset):
-    def __init__(self, folderName, n_draw, parameters_of_interest, cells_type=True, cells_density=True, oxygen=True,
-                 glucose=True):
-        self.cells_type = cells_type
-        self.cells_density = cells_density
-        self.oxygen = oxygen
-        self.glucose = glucose
+    def __init__(self, folderName, n_draw, parameters_of_interest, img_types=None):
+        if img_types is None:
+            img_types = [CELLS_TYPES, CELLS_DENSITIES, OXYGEN, GLUCOSE]
+        self.img_types = img_types
+        self.n_draw = n_draw
         self.parameters_of_interest = parameters_of_interest
-        s, i, d = re.findall(r'\d+', folderName)
+        s, i, d, h, w = re.findall(r'\d+', folderName)
+        self.height = int(h)
+        self.width = int(w)
         self.sequence_times = list(range(int(s), int(s) + int(d) * int(i), int(i)))[:n_draw]
         self.general_path = os.path.join(DATASET_FOLDER_PATH, folderName)
         self.dataframe = pd.read_csv(os.path.join(self.general_path, DATASET_FILE_NAME))
@@ -30,26 +32,25 @@ class SimulationDataset(Dataset):
     def __len__(self):
         return self.dataframe.shape[0]
 
-    def __getitem__(self, idx):
-        inputs = {}
-        for sequence_time in self.sequence_times:
-            inputs[sequence_time] = {}
-            if self.cells_type:
-                inputs[sequence_time][CELLS_TYPES] = np.load(
-                    os.path.join(self.general_path, DATA_NAME.format(idx, CELLS_TYPES, sequence_time)))
-            if self.cells_density:
-                inputs[sequence_time][CELLS_DENSITIES] = np.load(
-                    os.path.join(self.general_path, DATA_NAME.format(idx, CELLS_DENSITIES, sequence_time)))
-            if self.oxygen:
-                inputs[sequence_time][OXYGEN] = np.load(
-                    os.path.join(self.general_path, DATA_NAME.format(idx, OXYGEN, sequence_time)))
-            if self.glucose:
-                inputs[sequence_time][GLUCOSE] = np.load(
-                    os.path.join(self.general_path, DATA_NAME.format(idx, GLUCOSE, sequence_time)))
+    def get_height(self):
+        return self.height
 
-        outputs = {}
+    def get_width(self):
+        return self.width
+
+    def __getitem__(self, idx):
+        inputs = np.zeros((self.n_draw, len(self.img_types), self.height, self.width), dtype=np.float32)
+        for draw in range(self.n_draw):
+            for index_type in range(len(self.img_types)):
+                inputs[draw][index_type] = np.load(
+                    os.path.join(self.general_path,
+                                 DATA_NAME.format(idx, self.img_types[index_type], self.sequence_times[draw])))
+
+        outputs = np.zeros((len(self.parameters_of_interest)),dtype=np.float32)
+        i = 0
         for parameter in self.parameters_of_interest:
-            outputs[parameter] = self.dataframe.loc[idx][parameter]
+            outputs[i] = self.dataframe.loc[idx][parameter]
+            i += 1
 
         return inputs, outputs
 
