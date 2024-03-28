a9b3a1978f1a80bf0c3e08a1bf0e312d57c5053a
nicolas <nicolaspermanne@gmail.com>
2024-03-28 11:18:57 +0100

    More modular main.py

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import yaml
 from networks.model import Network
+import argparse
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-config', type=str, default='cell_cycle_study')
+parser.add_argument('-train', action='store_true')
 
 if __name__ == '__main__':
-    file = open("configurations/cell_cycle_study.yaml")
+
+    parser = parser.parse_args()
+    file = open(os.path.join("configurations", f"{parser.config}.yaml"))
     param = yaml.safe_load(file)
+
     network = Network(param)
-    network.train()
-    # network.load_weights()
+    if parser.train:
+        network.train()
+    else:
+        network.load_weights()
     network.evaluate()
