dc0ae5a49505c9e157753eada2578c7e8c12e907
nicolas <nicolaspermanne@gmail.com>
2024-03-28 17:56:29 +0100

    LSTM layer and CONV layer as hyperparameter

---
+++ networks/architecture.py
@@ -0,0 +1,72 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+######################################################################################
+#
+# CLASS DESCRIBING THE ARCHITECTURE
+#
+######################################################################################
+
+class Net(nn.Module):
+    def __init__(self, param):
+        super().__init__()
+
+        # Parameters
+        self.n_draws = param["N_DRAWS"]
+        self.n_types = param["N_TYPES"]
+        self.input_LSTM = param["INPUT_LSTM"]
+        self.output_LSTM = param["OUTPUT_LSTM"]
+        self.lstm_layers = param["LSTM_LAYERS"]
+        self.batch_size = param["BATCH_SIZE"]
+        self.height = param["HEIGHT"]
+        self.width = param["WIDTH"]
+        self.n_params = param["N_PARAMS"]
+        self.convolution_layers = param["CONV_LAYERS"]
+
+        # --------------------------------------------------------------------------------------
+        # CNN
+        self.convolutions = [
+            nn.Conv2d(self.n_types, 64, kernel_size=3, padding=1) if i == 0
+            else nn.Conv2d(64*(2**(i-1)), 64*(2**i), kernel_size=3, padding=1) for i in range(self.convolution_layers)]
+        self.pools = [nn.MaxPool2d(kernel_size=2, stride=2) for _ in range(self.convolution_layers)]
+
+        # size: (16/2^(conv_layer-1) x Height x Width)
+        self.linear1 = nn.Linear(int(16/(2**(self.convolution_layers-1))) * self.height * self.width, self.input_LSTM)
+        # size: (input_LSTM)
+
+        # --------------------------------------------------------------------------------------
+        # LSTM
+
+        # size: (input_LSTM)
+        self.lstm = nn.LSTM(self.input_LSTM, self.output_LSTM, batch_first=True, num_layers=self.lstm_layers)
+        # size: (output_LSTM)
+
+        # --------------------------------------------------------------------------------------
+        # FULLY CONNECTED
+
+        # size: (n_draws, output_LSTM)
+        # size: (n_draws x output_LSTM)
+        self.linear2 = nn.Linear(self.n_draws * self.output_LSTM, self.n_params)
+        # size: (n_params)
+
+    def forward(self, x):
+        # (Batch Size, n_draws, n_types, Height,  Width)
+        x = x.view(self.batch_size * self.n_draws, self.n_types, self.height, self.width)
+        # (Batch Size x n_draws, n_types, Height,  Width)
+        for i in range(self.convolution_layers):
+            x = F.relu(self.convolutions[i](x))
+            # (Batch Size x n_draws, 64*2^i, Height/(2^i),  Width/(2^i)
+            x = F.relu(self.pools[i](x))
+            # (Batch Size x n_draws, 64*2^i, Height/(2^(i+1)),  Width/(2^(i+1))
+        x = x.view(self.batch_size, self.n_draws, int(16/(2**(self.convolution_layers-1))) * self.height * self.width)
+        # (Batch Size x n_draws, 16/2^(conv_layer-1) x Height x Width)
+        x = F.relu(self.linear1(x))
+        # (Batch Size, n_draws, input_LSTM)
+        x, (h, c) = self.lstm(x)
+        # (Batch Size, n_draws, output_LSTM)
+        x = x.reshape(self.batch_size, self.n_draws * self.output_LSTM)
+        # (Batch Size, n_draws * output_LSTM)
+        x = F.sigmoid(self.linear2(x))
+        # (Batch Size, n_params)
+        return x
