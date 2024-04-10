55e770c996eb3fdec7d58481a6470899b31b2135
nicolas <nicolaspermanne@gmail.com>
2024-04-10 13:27:56 +0200

    Correction to similarity_study.py

--- simulation_similarity_analysis/similarity_study.py
+++ simulation_similarity_analysis/similarity_study.py
@@ -4,6 +4,8 @@ from multiprocessing import Pool
 from multiprocessing import set_start_method
 import time
 
+import numpy as np
+
 from utils import *
 
 SIMILARITY_ANALYSIS_FOLDER = "simulation_similarity_analysis"
@@ -29,13 +31,15 @@ class SimilarityAnalysis:
             v_min = np.min(datas)
             v_max = np.max(datas)
             for index, difference in enumerate(differences):
-                ax[index].set_title(f"{difference}")
+                title = f"{difference:2.3f}" if type(difference) == float else f"{difference}"
+                ax[index].set_title(title)
                 ax[index].imshow(datas[index], vmin=v_min, vmax=v_max)
                 ax[index].axis('off')
             plt.tight_layout()
             save_path = os.path.join(self.path,
-                                     f"Evolution of the mean difference between 2 images of types {images_type}.png")
+                                     f"Evolution of the mean difference between 2 images of types {images_type}  at time {draw}.png")
             plt.savefig(save_path, dpi=150, bbox_inches='tight')
+            plt.close()
 
     def mean_different_metric(self, metric: Metric, iteration, differences):
         fig, ax = plt.subplots(1, 4, dpi=150, figsize=(16, 2))
@@ -55,21 +59,60 @@ class SimilarityAnalysis:
         save_path = os.path.join(self.path,
                                  f"Mean of the {metric} between 2 images in function of the parameter difference.png")
         plt.savefig(save_path, dpi=150, bbox_inches='tight')
+        plt.close()
 
 
 if __name__ == '__main__':
+    # average_cancer_glucose_absorption
     analysis = SimilarityAnalysis(
-        "cell_cycle",
-        0,
+        "average_cancer_glucose_absorption",
+        0.005,
         "same_study_start=350_interval=100_ndraw=8_size=(64,64)",
-        "cell_cycle_start=350_interval=100_ndraw=8_size=(64,64)")
+        "average_cancer_glucose_absorption_study_start=350_interval=100_ndraw=8_size=(64,64)")
 
     i = 0
-    analysis.mean_different_image(350, 1000, [0, 5, 10, 15, 20])
+    analysis.mean_different_image(350, 1000, np.linspace(0, 1, 5))
+    analysis.mean_different_image(950, 1000, np.linspace(0, 1, 5))
     i += 1
     print(f"{i}/6", end='\r')
     for metric in [Metric.CORRELATION_HISTOGRAM, Metric.SSIM, Metric.MEAN_ABSOLUTE_ERROR,
                    Metric.ROOT_MEAN_SQUARED_ERROR, Metric.MAX_ABSOLUTE_ERROR]:
-        analysis.mean_different_metric(metric, 1000, range(0, 21))
+        analysis.mean_different_metric(metric, 1000, np.linspace(0, 1, 21))
+        i += 1
+        print(f"{i}/6", end='\r')
+
+    # average_healthy_oxygen_consumption
+    analysis = SimilarityAnalysis(
+        "average_healthy_oxygen_consumption",
+        0.5,
+        "same_study_start=350_interval=100_ndraw=8_size=(64,64)",
+        "average_healthy_oxygen_consumption_study_start=350_interval=100_ndraw=8_size=(64,64)")
+
+    i = 0
+    analysis.mean_different_image(350, 1000, np.linspace(0, 40, 5))
+    analysis.mean_different_image(950, 1000, np.linspace(0, 40, 5))
+    i += 1
+    print(f"{i}/6", end='\r')
+    for metric in [Metric.CORRELATION_HISTOGRAM, Metric.SSIM, Metric.MEAN_ABSOLUTE_ERROR,
+                   Metric.ROOT_MEAN_SQUARED_ERROR, Metric.MAX_ABSOLUTE_ERROR]:
+        analysis.mean_different_metric(metric, 1000, np.linspace(0, 40, 21))
+        i += 1
+        print(f"{i}/6", end='\r')
+
+    # average_cancer_oxygen_consumption
+    analysis = SimilarityAnalysis(
+        "average_cancer_oxygen_consumption",
+        0.5,
+        "same_study_start=350_interval=100_ndraw=8_size=(64,64)",
+        "average_cancer_oxygen_consumption_study_start=350_interval=100_ndraw=8_size=(64,64)")
+
+    i = 0
+    analysis.mean_different_image(350, 1000, np.linspace(0, 40, 5))
+    analysis.mean_different_image(950, 1000, np.linspace(0, 40, 5))
+    i += 1
+    print(f"{i}/6", end='\r')
+    for metric in [Metric.CORRELATION_HISTOGRAM, Metric.SSIM, Metric.MEAN_ABSOLUTE_ERROR,
+                   Metric.ROOT_MEAN_SQUARED_ERROR, Metric.MAX_ABSOLUTE_ERROR]:
+        analysis.mean_different_metric(metric, 1000, np.linspace(0, 40, 21))
         i += 1
         print(f"{i}/6", end='\r')
