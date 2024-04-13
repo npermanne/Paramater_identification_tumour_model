e5eb6e87a24b9edad8a97395fcd2f8373388877d
nicolas <nicolaspermanne@gmail.com>
2024-04-13 15:21:48 +0200

    class for early stopping

---
+++ networks/early_stopping.py
@@ -0,0 +1,18 @@
+
+# https://stackoverflow.com/questions/71998978/early-stopping-in-pytorch
+class EarlyStopper:
+    def __init__(self, patience=10, min_delta=0.001):
+        self.patience = patience
+        self.min_delta = min_delta
+        self.counter = 0
+        self.min_validation_loss = float('inf')
+
+    def __call__(self, validation_loss) -> bool:
+        if validation_loss < self.min_validation_loss:
+            self.min_validation_loss = validation_loss
+            self.counter = 0
+        elif validation_loss > (self.min_validation_loss + self.min_delta):
+            self.counter += 1
+            if self.counter >= self.patience:
+                return True
+        return False
