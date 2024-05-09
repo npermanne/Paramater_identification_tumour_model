79832d0542f761b136afc61f8e53886640923dff
nicolas <nicolaspermanne@gmail.com>
2024-05-09 21:55:47 +0200

    update run all config

--- run_all_config.py
+++ run_all_config.py
@@ -5,10 +5,11 @@ import os
 if __name__ == '__main__':
     config_path = "configurations"
 
-    for filename in os.listdir(config_path)[:-1]:
-        file = open(os.path.join("configurations", filename))
-        param = yaml.safe_load(file)
-        param["NAME"] = filename[:-5]
-        network = Network(param)
-        network.train()
-        network.evaluate()
+    for filename in os.listdir(config_path):
+        if filename.endswith(".yaml"):
+            file = open(os.path.join("configurations", filename))
+            param = yaml.safe_load(file)
+            param["NAME"] = filename[:-5]
+            network = Network(param)
+            network.train()
+            network.evaluate()
